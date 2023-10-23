8e851c6c6889ce5f44a3e67645063d2e216bf429
Caroline <C.klakegg@gmail.com>
2023-10-23 08:22:10 +0200

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Caroline Klakegg on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
